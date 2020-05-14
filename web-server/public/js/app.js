0bd4ffec6669d7af2cf402372825b792aec9ed26
pachinko <sunpochin@gmail.com>
2020-05-15 00:48:10 +0800

    Setup for heroku.

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -3,6 +3,11 @@ console.log('Client side javascript file is loaded!')
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const message1 = document.querySelector('#message-1')
+const message2 = document.querySelector('#message-2')
+
+message1.textContent = 'LOADING!!!'
+message2.textContent = '.............'
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -10,14 +15,17 @@ weatherForm.addEventListener('submit', (e) => {
     const location = search.value
     console.log('testing! :', location)
 
-    let url = 'http://localhost:3000/weather?address=' + location
+    let url = '/weather?address=' + location
     fetch(url).then(
         (response) => {response.json().then( (data) => {
             if (data.error) {
                 console.log('data.error: ',data.error)
+                message1.textContent = data.error
             } else {
                 console.log('data.location: ', data.location)
                 console.log('data.forecast: ', data.forecast)
+                message1.textContent = data.location
+                message2.textContent = data.forecast
             }
         })
     })
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -11,6 +11,8 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
 
+const port = process.env.PORT || 3000
+
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
@@ -22,14 +24,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Andrew Mead'
+        name: 'Sun Pochin'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Andrew Mead'
+        name: 'Sun Pochin'
     })
 })
 
@@ -37,7 +39,7 @@ app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text.',
         title: 'Help',
-        name: 'Andrew Mead'
+        name: 'Sun Pochin'
     })
 })
 
@@ -85,7 +87,7 @@ app.get('/products', (req, res) => {
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Sun Pochin',
         errorMessage: 'Help article not found.'
     })
 })
@@ -93,11 +95,11 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Sun Pochin',
         errorMessage: 'Page not found.'
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port: ', port)
 })

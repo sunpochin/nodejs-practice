d21c66c33d6e0b6405171adf3b48f6475972a08d
pachinko <sunpochin@github.com>
2020-10-25 00:30:33 +0800

    changing to dynamic web page

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -22,30 +22,31 @@ hbs.registerPartials(partialsPath)
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
-//    res.send('hello express pac')
-    res.send('<h1>Weather ! </h1>')
-//     res.render('index', {
-//         title: 'Weather',
-//         name: 'Sun Pochin'
-//     })
+//    res.send('<h1>Weather ! </h1>')
+//     res.render('index')
+
+    res.render('index', {
+        title: 'Weather',
+        name: 'Sun Pochin'
+    })
 })
 
 
-// app.get('/about', (req, res) => {
-//     res.send('about')
-//     // res.render('about', {
-//     //     title: 'About Me',
-//     //     name: 'Sun Pochin'
-//     // })
-// })
+app.get('/about', (req, res) => {
+//    res.send('about')
+    res.render('about', {
+        title: 'About Me',
+        name: 'Sun Pochin'
+    })
+})
 
-// app.get('/help', (req, res) => {
-//     res.render('help', {
-//         helpText: 'This is some helpful text.',
-//         title: 'Help',
-//         name: 'Sun Pochin'
-//     })
-// })
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.',
+        title: 'Help',
+        name: 'Sun Pochin'
+    })
+})
 
 // app.get('/weather', (req, res) => {
 //     if (!req.query.address) {
@@ -88,21 +89,21 @@ app.get('', (req, res) => {
 //     })
 // })
 
-// app.get('/help/*', (req, res) => {
-//     res.render('404', {
-//         title: '404',
-//         name: 'Sun Pochin',
-//         errorMessage: 'Help article not found.'
-//     })
-// })
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Sun Pochin',
+        errorMessage: 'Help article not found.'
+    })
+})
 
-// app.get('*', (req, res) => {
-//     res.render('404', {
-//         title: '404',
-//         name: 'Sun Pochin',
-//         errorMessage: 'Page not found.'
-//     })
-// })
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Sun Pochin',
+        errorMessage: 'Page not found.'
+    })
+})
 
 app.listen(port, () => {
     console.log('Server is up on port: ', port)

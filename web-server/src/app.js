e2fea40df90297f2247cfde09c74d15474d0efe0
pachinko <sunpochin@github.com>
2020-10-24 16:59:26 +0800

    adding static html temporarily

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -22,84 +22,89 @@ hbs.registerPartials(partialsPath)
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
-    res.render('index', {
-        title: 'Weather',
-        name: 'Sun Pochin'
-    })
+//    res.send('hello express pac')
+    res.send('<h1>Weather ! </h1>')
+//     res.render('index', {
+//         title: 'Weather',
+//         name: 'Sun Pochin'
+//     })
 })
 
-app.get('/about', (req, res) => {
-    res.render('about', {
-        title: 'About Me',
-        name: 'Sun Pochin'
-    })
-})
 
-app.get('/help', (req, res) => {
-    res.render('help', {
-        helpText: 'This is some helpful text.',
-        title: 'Help',
-        name: 'Sun Pochin'
-    })
-})
+// app.get('/about', (req, res) => {
+//     res.send('about')
+//     // res.render('about', {
+//     //     title: 'About Me',
+//     //     name: 'Sun Pochin'
+//     // })
+// })
 
-app.get('/weather', (req, res) => {
-    if (!req.query.address) {
-        return res.send({
-            error: 'You must provide an address!'
-        })
-    }
+// app.get('/help', (req, res) => {
+//     res.render('help', {
+//         helpText: 'This is some helpful text.',
+//         title: 'Help',
+//         name: 'Sun Pochin'
+//     })
+// })
 
-    geocode(req.query.address, (error, { latitude, longitude, location } = {} ) => {
-        if (error) {
-            console.log('error 25: ', error)
-            return res.send({ error })
-        }
-        console.log('latitude: ' + latitude + ', longitude: ' + longitude)
-        forecast(latitude, longitude, (error, forecastData) => {
-            if (error) {
-                console.log('error 24: ', error)
-                return res.send({ error })
-            }
+// app.get('/weather', (req, res) => {
+//     if (!req.query.address) {
+//         return res.send({
+//             error: 'You must provide an address!'
+//         })
+//     }
 
-            res.send({
-                forecast: forecastData,
-                location,
-                address: req.query.address
-            })
-        })
-    })
-})
+//     geocode(req.query.address, (error, { latitude, longitude, location } = {} ) => {
+//         if (error) {
+//             console.log('error 25: ', error)
+//             return res.send({ error })
+//         }
+//         console.log('latitude: ' + latitude + ', longitude: ' + longitude)
+//         forecast(latitude, longitude, (error, forecastData) => {
+//             if (error) {
+//                 console.log('error 24: ', error)
+//                 return res.send({ error })
+//             }
 
-app.get('/products', (req, res) => {
-    if (!req.query.search) {
-        return res.send({
-            error: 'You must provide a search term'
-        })
-    }
+//             res.send({
+//                 forecast: forecastData,
+//                 location,
+//                 address: req.query.address
+//             })
+//         })
+//     })
+// })
 
-    console.log(req.query.search)
-    res.send({
-        products: []
-    })
-})
+// app.get('/products', (req, res) => {
+//     if (!req.query.search) {
+//         return res.send({
+//             error: 'You must provide a search term'
+//         })
+//     }
 
-app.get('/help/*', (req, res) => {
-    res.render('404', {
-        title: '404',
-        name: 'Sun Pochin',
-        errorMessage: 'Help article not found.'
-    })
-})
+//     console.log(req.query.search)
+//     res.send({
+//         products: []
+//     })
+// })
 
-app.get('*', (req, res) => {
-    res.render('404', {
-        title: '404',
-        name: 'Sun Pochin',
-        errorMessage: 'Page not found.'
-    })
-})
+// app.get('/help/*', (req, res) => {
+//     res.render('404', {
+//         title: '404',
+//         name: 'Sun Pochin',
+//         errorMessage: 'Help article not found.'
+//     })
+// })
+
+// app.get('*', (req, res) => {
+//     res.render('404', {
+//         title: '404',
+//         name: 'Sun Pochin',
+//         errorMessage: 'Page not found.'
+//     })
+// })
 
 app.listen(port, () => {
     console.log('Server is up on port: ', port)
 })
+
--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -1,13 +1,13 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    let YOUR_ACCESS_KEY = '4a611cbfd8889b0b317701c86b7caeab'
+    var YOUR_ACCESS_KEY = '4a611cbfd8889b0b317701c86b7caeab'
 //    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
     const url = 'http://api.weatherstack.com/current?access_key=' + YOUR_ACCESS_KEY 
         + '&query=' + latitude + ',' + longitude
     // console.log('forecast url:', url)
 
-    request({ url, json: true }, (error, { body }) => {
+    request({ url, json: true }, (error, res, data) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
